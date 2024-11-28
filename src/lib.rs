3d4924c051922907560d44c16285fb6965111dbd
Think Wu <yingxinwu.g@gmail.com>
2024-11-28 23:46:43 +0800

    add ohos sample

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,7 @@
-use std::os::raw::c_char;
-use std::ffi::{CString, CStr};
-
 // The shared function
-pub fn add(left: i64, right: i64) -> i64 {
-    left + right
-}
+// pub fn add(left: i64, right: i64) -> i64 {
+//     left + right
+// }
 
 #[cfg(test)]
 mod tests {
@@ -37,28 +34,13 @@ pub extern "system" fn Java_com_example_rustsample_MathLib_add__JJ(
     add(left, right)
 }
 
-#[cfg(feature = "ohos")]
-use oh_napi_sys::{
-    napi_env,
-    napi_value,
-    napi_callback_info,
+// #[cfg(feature = "ios")]
+use std::{
+    os::raw::c_char,
+    ffi::{CString, CStr},
 };
 
-#[cfg(feature = "ohos")]
-pub extern "C" fn addd(env: napi_env, info: napi_callback_info) -> napi_value {
-    unsafe {
-        // let env = Env::from(env);
-
-        let args = env.get_args(info);
-
-        let a = env.get_int64_from(args[0]);
-        let b = env.get_int64_from(args[1]);
-        let c = add(a, b);
-
-        env.create_int64(c)
-    }
-}
-
+// #[cfg(feature = "ios")]
 #[no_mangle]
 pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
     let c_str = unsafe { CStr::from_ptr(to) };
@@ -70,6 +52,7 @@ pub extern fn rust_greeting(to: *const c_char) -> *mut c_char {
     CString::new("Hello ".to_owned() + recipient + "!").unwrap().into_raw()
 }
 
+// #[cfg(feature = "ios")]
 #[no_mangle]
 pub extern fn rust_greeting_free(s: *mut c_char) {
     unsafe {
@@ -77,3 +60,16 @@ pub extern fn rust_greeting_free(s: *mut c_char) {
         CString::from_raw(s)
     };
 }
+
+// use napi_derive_ohos::napi;
+
+// #[napi]
+pub fn add(left: i64, right: i64) -> i64 {
+    left + right
+}
+
+// #[link(name = "ace_napi.z")]
+// #[link(name = "ace_ndk.z")]
+// extern "C" {
+
+// }
